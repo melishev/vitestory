ffbfbdc0c96d84534206b2aa07e6ca4ee12d2275
Matvey Melishev <melishevm@gmail.com>
2024-11-03 01:05:09 +0100

    chore: remove old code, replace to default vite starter template

--- apps/example/docs/playbox-example.md
+++
@@ -1,7 +0,0 @@
-# Markdown with Playbox example
-
-<script setup>
-import Playbox from '../src/playbox.vue'
-</script>
-
-<Playbox />
--- apps/example/src/app.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Preview from './playbox.vue';
-</script>
-
-<template>
-  <div>
-    <Preview />
-  </div>
-</template>
