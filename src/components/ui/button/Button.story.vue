2b91001853b4dedb940ffc59b38847973e9db70f
Matvey Melishev <melishevm@gmail.com>
2024-11-04 19:16:03 +0100

    style: format all code

--- src/components/ui/button/Button.story.vue
+++ src/components/ui/button/Button.story.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import type { ViteStoryExposeOptions } from 'vitestory'
 import { VSVariant } from 'vitestory/components'
-import Button from './Button.vue';
-import { Input } from '../input';
-import { Switch } from '../switch';
+import Button from './Button.vue'
+import { Input } from '../input'
+import { Switch } from '../switch'
 
 import { ChevronRight, Mail, Loader2 } from 'lucide-vue-next'
 
@@ -32,33 +32,23 @@ defineExpose<ViteStoryExposeOptions>({
   </VSVariant>
 
   <VSVariant title="Secondary">
-    <Button variant="secondary">
-      Secondary
-    </Button>
+    <Button variant="secondary"> Secondary </Button>
   </VSVariant>
 
   <VSVariant title="Destructive">
-    <Button variant="destructive">
-      Destructive
-    </Button>
+    <Button variant="destructive"> Destructive </Button>
   </VSVariant>
 
   <VSVariant title="Outline">
-    <Button variant="outline">
-      Outline
-    </Button>
+    <Button variant="outline"> Outline </Button>
   </VSVariant>
 
   <VSVariant title="Ghost">
-    <Button variant="ghost">
-      Ghost
-    </Button>
+    <Button variant="ghost"> Ghost </Button>
   </VSVariant>
 
   <VSVariant title="Link">
-    <Button variant="link">
-      Link
-    </Button>
+    <Button variant="link"> Link </Button>
   </VSVariant>
 
   <VSVariant title="Icon">
@@ -68,9 +58,7 @@ defineExpose<ViteStoryExposeOptions>({
   </VSVariant>
 
   <VSVariant title="With Icon">
-    <Button>
-      <Mail class="w-4 h-4 mr-2" /> Login with Email
-    </Button>
+    <Button> <Mail class="w-4 h-4 mr-2" /> Login with Email </Button>
   </VSVariant>
 
   <VSVariant title="Loading">
@@ -82,9 +70,7 @@ defineExpose<ViteStoryExposeOptions>({
 
   <VSVariant title="As Child">
     <Button as-child>
-      <a href="/login">
-        Login
-      </a>
+      <a href="/login"> Login </a>
     </Button>
   </VSVariant>
 </template>
